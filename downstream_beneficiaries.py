3e1a0179a874428594c41d8a8f76537523723b88
Rich Sharp <richpsharp@gmail.com>
2021-04-12 16:44:20 -0700

    just warn

--- downstream_beneficiaries.py
+++ downstream_beneficiaries.py
@@ -34,7 +34,7 @@ logging.basicConfig(
         '%(asctime)s (%(relativeCreated)d) %(processName)s %(levelname)s '
         '%(name)s [%(funcName)s:%(lineno)d] %(message)s'))
 LOGGER = logging.getLogger(__name__)
-logging.getLogger('taskgraph').setLevel(logging.INFO)
+logging.getLogger('taskgraph').setLevel(logging.WARN)
 logging.getLogger('pygeoprocessing').setLevel(logging.WARN)
 
 
